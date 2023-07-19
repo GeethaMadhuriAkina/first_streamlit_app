73430444d23855f2c3ce3821e3e326f4858bcf45
GeethaMadhuriAkina <50434689+GeethaMadhuriAkina@users.noreply.github.com>
2023-07-20 00:01:02 +0100

    Update streamlit_app.py
    added line of code for the header 
    added lines for breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 streamlit.title('My Parents Healthy New Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 and blueberry oatmeal')
+streamlit.text('French toast with whipped cream and berries')
+streamlit.text('Banana pancakes with maple syrup')
 
