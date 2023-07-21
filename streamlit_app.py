d6aa2b608e837688e3339f2722529c003aa449a2
GeethaMadhuriAkina <50434689+GeethaMadhuriAkina@users.noreply.github.com>
2023-07-21 15:07:12 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ except URLError as e:
       streamlit.error()
 
 #snowflake related functions
-streamlit.header("View Our Fruit List.Add Your Favourites!!!")
+streamlit.header("View Our Fruit List - Add Your Favourites!!!")
 
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
